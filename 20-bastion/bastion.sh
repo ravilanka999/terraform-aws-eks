f5136034b6c8e6ff18ac89f25b4e8aad4740f41b
Ravikumar Lanka <ravi050892@gmail.com>
2025-08-21 09:17:56 +0530

    jenkins

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -33,3 +33,7 @@ kubectl version
 
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
